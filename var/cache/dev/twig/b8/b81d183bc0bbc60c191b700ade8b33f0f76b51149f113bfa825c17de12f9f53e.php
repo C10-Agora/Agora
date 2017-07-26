324940ac060565955062d30b42b00748602f7ea9
phally <sphally.kim@edge-ict.com>
2017-07-26 18:59:33 +0200

    initialisation du projet

---
+++ var/cache/dev/twig/b8/b81d183bc0bbc60c191b700ade8b33f0f76b51149f113bfa825c17de12f9f53e.php
@@ -0,0 +1,58 @@
+<?php
+
+/* C10AgoraBundle:Default:index.html.twig */
+class __TwigTemplate_4af83ca3bad002bcfbc8d424979fc17be883d489ee38f16085272ea3b11325f2 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_5da07adb2b1a884e25dcd8d782db9ef4c4fc406eec6981836ef0746ca669c4df = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_5da07adb2b1a884e25dcd8d782db9ef4c4fc406eec6981836ef0746ca669c4df->enter($__internal_5da07adb2b1a884e25dcd8d782db9ef4c4fc406eec6981836ef0746ca669c4df_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "C10AgoraBundle:Default:index.html.twig"));
+
+        $__internal_6efd04272bf9647134e5afa8d98008432a9b86c8c76a72de36743dbd91168e36 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_6efd04272bf9647134e5afa8d98008432a9b86c8c76a72de36743dbd91168e36->enter($__internal_6efd04272bf9647134e5afa8d98008432a9b86c8c76a72de36743dbd91168e36_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "C10AgoraBundle:Default:index.html.twig"));
+
+        // line 1
+        echo "Hello World!
+";
+        
+        $__internal_5da07adb2b1a884e25dcd8d782db9ef4c4fc406eec6981836ef0746ca669c4df->leave($__internal_5da07adb2b1a884e25dcd8d782db9ef4c4fc406eec6981836ef0746ca669c4df_prof);
+
+        
+        $__internal_6efd04272bf9647134e5afa8d98008432a9b86c8c76a72de36743dbd91168e36->leave($__internal_6efd04272bf9647134e5afa8d98008432a9b86c8c76a72de36743dbd91168e36_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "C10AgoraBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  25 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("Hello World!
+", "C10AgoraBundle:Default:index.html.twig", "D:\\home\\site\\wwwroot\\src\\C10\\AgoraBundle/Resources/views/Default/index.html.twig");
+    }
+}
